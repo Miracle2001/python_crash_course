7ed45ee0cf2c78eea3605000eafb49bb3198b406
Miracle Ugwu <47611584+Miracle2001@users.noreply.github.com>
2025-12-13 20:52:31 +0000

    first exercise completed

---
+++ chapter_2/hello_world.py
@@ -0,0 +1,6 @@
+print("Hello Python World!")
+
+message = "Hello Python World!"
+print(message)
+
+##print(mesage)
---
+++ chapter_2/simple_message.py
@@ -0,0 +1,5 @@
+message = 'I have a huge potential'
+print(message)
+
+message = 'I havent began to unlock what i can do '
+print(message)
