14894769d49ca186cc905fe0940c55f37a6dff5e
Miracle Ugwu <47611584+Miracle2001@users.noreply.github.com>
2025-12-14 20:36:36 +0000

    more pratice

---
+++ chapter_2/exercise_2.3-2.8.py
@@ -0,0 +1,17 @@
+#### 1) Exercise 2-3: Personal Message
+name = 'Mimi'
+print(f'Hello {name}, would you like to learn some Python today?')
+
+print('\n')
+#### 2) Exercise 2-4: Name Cases
+name = 'Mimi'
+last_name = 'Jazz'
+
+full_name =f'{name} {last_name}'
+print(full_name.title())
+print(full_name.upper())
+print(full_name.lower())
+print('\n')
+
+#### 3) Exercise 2-5: Famous Quote
+print('Albert Einstein once said, "A person who never made a mistake never tried anything new".')
---
+++ chapter_2/full_name.py
@@ -0,0 +1,9 @@
+first_name = 'ada'
+last_name = 'love'
+full_name = f'{first_name} {last_name}'
+print(f'Hello {full_name.title()}!')
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+nostarch_url = 'https://nostarch.com'
+print(nostarch_url.removeprefix('https://'))
